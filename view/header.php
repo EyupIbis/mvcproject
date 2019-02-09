0b114e9166e8798dead482ec628cc436efa52659
EyupIbis <44265485+EyupIbis@users.noreply.github.com>
2019-02-09 10:03:56 +0100

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,44 +1,40 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link href= "assets/css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo BASE_URL . "index.php?action=home"; ?>" #">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo BASE_URL . "index.php?action=webdev"; ?>">webdev</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo BASE_URL . "index.php?action=company"; ?>">company</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo BASE_URL . "index.php?action=interview"; ?>">interview</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo BASE_URL . "index.php?action=conclusion"; ?>">company</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?action=makingOf">Making of</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?action=bedrijf">Bedrijf</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?action=webdev">Webdev</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?action=interview">Interview</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?action=conclusie">Conclusie</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+<div class="container">
